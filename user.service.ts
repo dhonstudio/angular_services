f1af93b9bd62a58f8c4a400031a13bca1cad738e
dhonstudio <doon13@gmail.com>
2022-02-09 04:07:47 +0700

    Permission for number type on user service

--- user.service.ts
+++ user.service.ts
@@ -14,12 +14,12 @@ export class UserService {
     private globalService: GlobalService,
   ) { }
 
-  async checkUser(db: string, table: string, auth: {username: string, password: string}, user: string, user_field_name: string): Promise<User[]> {
+  async checkUser(db: string, table: string, auth: {username: string, password: string}, user: string | number, user_field_name: string): Promise<User[]> {
     let get = `${user_field_name}=${user}`
     return (await firstValueFrom(this.httpClient.get<any>(`${this.globalService.apiUrl}/${db}/${table}?${get}`, this.globalService.setHttpOptions({username:auth.username, password: auth.password})))).data;
   }
 
-  async checkPassword(db: string, table: string, auth: {username: string, password: string}, user: string, user_field_name: string, password: string, password_field_name: string): Promise<User[]> {
+  async checkPassword(db: string, table: string, auth: {username: string, password: string}, user: string | number, user_field_name: string, password: string, password_field_name: string): Promise<User[]> {
     let get = `${user_field_name}=${user}&${password_field_name}=${password}`
     return (await firstValueFrom(this.httpClient.get<any>(`${this.globalService.apiUrl}/${db}/${table}/password_verify?${get}`, this.globalService.setHttpOptions({username:auth.username, password: auth.password})))).data;
   }
